d8170ad9aa9d0c227a318ffcfe3319c23f2d99f2
Fan-tome14 <96857821+Fan-tome14@users.noreply.github.com>
2025-10-16 09:06:43 -0400

    commit

---
+++ Code/Source/TowerDefense/MyAssetActions.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetActionUtility.h"
+#include "MyAssetActions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API UMyAssetActions : public UAssetActionUtility
+{
+	GENERATED_BODY()
+	
+};
